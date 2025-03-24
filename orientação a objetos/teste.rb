3e9f0464fee3857874fc984cf23fbe24fa275d70
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2025-03-24 11:55:32 -0300

    exemplos do livro A linguagem de Programação Ruby

--- orientação a objetos/teste.rb
+++ orientação a objetos/teste.rb
@@ -20,3 +20,32 @@ end
 produto = Produto.new("JS8") 
 #produto.nome = "MotoG8"
 produto.mostrar("Motorola")
+
+
+# Exemplos do Livro A linguagem de Programação Ruby
+# Capítulo 7  Classes e Modulos
+
+class Point
+
+    def initialize(x,y) # construtor de um classe, automaticamente já é privado
+        @x, @y = x,y
+    end
+
+    def to_s         # retorna uma string que representa esse ponto
+        "(#@x,#@y)"  # Apenas interpola as variaveis de instancia em uma sequencia
+    end
+
+    def x 
+        @x
+    end
+
+    def y 
+        @y
+    end
+end
+
+p = Point.new(1,2) # um nova instância da classe Point
+q = Point.new(p.x*2, p.y*3)
+
+puts p
+
